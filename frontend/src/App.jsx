b3fbb8459680c494c94a3d9c49d2ddd1abb817fc
Hinata <115453542+HADIL19@users.noreply.github.com>
2026-02-06 14:57:00 +0100

    App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import About from './components/About';
-import Description from './components/Description';
-import Content from './components/Content';
-import Contact from './components/Contact';
-import Hackathons from './components/Hackathons';
-import Internships from './components/Internships';
-import Courses from './components/Courses';
+import Navbar from './components/Navbar.jsx';
+import About from './components/About.jsx';
+import Description from './components/Description.jsx';
+import Content from './components/Content.jsx';
+import Contact from './components/Contact.jsx';
+import Hackathons from './components/Hackathons.jsx';
+import Internships from './components/Internships.jsx';
+import Courses from './components/Courses.jsx';
 const App = () => {
   return (
    <Router>
